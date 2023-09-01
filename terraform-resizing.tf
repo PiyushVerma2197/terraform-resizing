a99e459c7c5c124a31cf8b09e52a478cb9260474
PiyushVerma2197 <118599199+PiyushVerma2197@users.noreply.github.com>
2023-09-01 15:40:00 +0530

    Update terraform-resizing.tf

--- terraform-resizing.tf
+++ terraform-resizing.tf
@@ -1,33 +1,39 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+import boto3
+from botocore.exceptions import WaiterError
 
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIAZNTW6QXGZSVO5KU5"
-  secret_key = "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"
-}
+def wait_until_instance_stopped(ec2_client, instance_id):
+    waiter = ec2_client.get_waiter('instance_stopped')
+    try:
+        waiter.wait(InstanceIds=[instance_id])
+    except WaiterError as e:
+        # Handle the exception if the instance fails to stop within the specified timeout
+        print(f"Error waiting for instance to stop: {e}")
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-south-1"
-}
+def lambda_handler(event, context):
+    # Retrieve the instance ID from the event parameter
+    instance_id = event['instanceId']
+    
+    # Create an EC2 client
+    ec2_client = boto3.client('ec2')
+    
+    try:
+        # Stop the instance
+        ec2_client.stop_instances(InstanceIds=[instance_id])
+        
+        # Wait until the instance is stopped
+        wait_until_instance_stopped(ec2_client, instance_id)
+        
+        # Modify the instance type
+        ec2_client.modify_instance_attribute(InstanceId=instance_id, InstanceType={'Value': 't2.medium'})
+        
+        # Start the instance
+        ec2_client.start_instances(InstanceIds=[instance_id])
+        
+        return 'Instance resizing complete.'
+    
+    except Exception as e:
+        # Handle any exceptions that occur during the resizing process
+        return f'Error resizing instance: {e}'
+    
 
-resource "aws_instance" "terraform" {
-  ami           = "ami-05dbd1926bfb06723"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "terraform"
-  }
-}
-
-resource "aws_ec2_instance_state" "terraform" {
-  instance_id = "i-00f9c34877b417374"
-  state       = "stopped"
-}
+# Test event JSON: {"instanceId": "<instance-ID>"}
